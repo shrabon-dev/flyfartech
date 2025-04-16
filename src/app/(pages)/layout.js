f0445091891567ea858ea279a1cf5203cb6d1123
shrabon-dev <shrabonhossain.dev@gmail.com>
2025-04-17 00:13:31 +0600

    solve all issue

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
           <Head>
-          <link rel="icon" href='/images/favicon62.png' />
+          <link rel="icon" href='../assets/images/favicon62.png' />
           </Head>
           <body className={`${geistSans.variable} ${geistMono.variable}`}>
             <Provider store={store} >
